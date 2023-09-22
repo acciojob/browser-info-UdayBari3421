c43873c8020b2e5bd8887d1ec78327737573b9c9
UdayBari3421 <115573575+UdayBari3421@users.noreply.github.com>
2023-09-22 10:19:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let domItem = document.getElementById("browser-info");
+
+let agent = navigator.userAgent;
+let app = navigator.appName;
+let appVersion = navigator.appVersion;
+
+domItem.innerHTML = agent + app + appVersion; 
